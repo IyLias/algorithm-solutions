6461e5f2ae8abe927b007a773dafc313f63a3eca
IyLias <48081162+IyLias@users.noreply.github.com>
2021-04-25 10:53:44 +0900

    solved combination sum but need revision
    solved combination sum but need revision

--- leetcode/Q39_combination_sum.py
+++ leetcode/Q39_combination_sum.py
@@ -7,11 +7,18 @@ class Solution:
             if target <= 0:
                if target == 0:
                   for result in results:
-                      if sorted(result) == sorted(elements)                                                                                                     return
+                      if sorted(result) == sorted(elements)     
+                            results.append(elements)
+            
+            return
                                                                                                                                                        results.append(elements[:])
-                                                                                                                                                    return 
-                                                                                                                                                                                                            
-                                                                                                                                                  for candidate in candidates:
-                 elements.append(candidate)                                                                                                           search(elements,target-candidate)
+            for candidate in candidates:                                                                                                                               for candidate in candidates:
+                 elements.append(candidate)
+                 search(elements,target-candidate)
                  elements.pop()
-                                                                                                                                                                                                                                                                                       search(elements,target)                                                                                                                                                                                                                                                 return results
+                
+        search(elements,target)                                                                                                                                                                                                                                                 return results
+        return results
+            
+    
+    
